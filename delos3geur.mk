eb26ef7dc1e8033c7e315ec7cff8b7ff6bba2afc
Arnav Gosain <nolinuxnoparty@gmail.com>
2013-12-01 15:52:08 +0530

    delos3geur: update delos3geur.mk

--- delos3geur.mk
+++ delos3geur.mk
@@ -17,99 +17,107 @@ LOCAL_PATH := device/samsung/delos3geur
 DEVICE_PACKAGE_OVERLAYS := device/samsung/delos3geur/overlay
 
 
-# These are the hardware-specific features
+# Hardware properties 
 PRODUCT_COPY_FILES += \
-	frameworks/native/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml \
-	frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:system/etc/permissions/android.hardware.camera.flash-autofocus.xml \
-	frameworks/native/data/etc/android.hardware.camera.front.xml:system/etc/permissions/android.hardware.camera.front.xml \
-	frameworks/native/data/etc/android.hardware.location.gps.xml:system/etc/permissions/android.hardware.location.gps.xml \
-	frameworks/native/data/etc/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml \
-	frameworks/native/data/etc/android.hardware.wifi.direct.xml:system/etc/permissions/android.hardware.wifi.direct.xml \
-	frameworks/native/data/etc/android.hardware.sensor.proximity.xml:system/etc/permissions/android.hardware.sensor.proximity.xml \
-	frameworks/native/data/etc/android.hardware.sensor.light.xml:system/etc/permissions/android.hardware.sensor.light.xml \
-	frameworks/native/data/etc/android.hardware.sensor.gyroscope.xml:system/etc/permissions/android.hardware.sensor.gyroscope.xml \
-	frameworks/native/data/etc/android.hardware.sensor.barometer.xml:system/etc/permissions/android.hardware.sensor.barometer.xml \
-	frameworks/native/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml \
-	frameworks/native/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml \
-	frameworks/native/data/etc/android.hardware.usb.accessory.xml:system/etc/permissions/android.hardware.usb.accessory.xml \
-	frameworks/native/data/etc/android.hardware.telephony.gsm.xml:system/etc/permissions/android.hardware.telephony.gsm.xml \
-	frameworks/native/data/etc/android.hardware.audio.low_latency.xml:system/etc/permissions/android.hardware.audio.low_latency.xml \
+    frameworks/native/data/etc/android.hardware.camera.xml:system/etc/permissions/android.hardware.camera.xml \
+    frameworks/native/data/etc/android.hardware.location.gps.xml:system/etc/permissions/android.hardware.location.gps.xml \
+    frameworks/native/data/etc/android.hardware.location.xml:system/etc/permissions/android.hardware.location.xml \
+    frameworks/native/data/etc/android.hardware.sensor.accelerometer.xml:system/etc/permissions/android.hardware.sensor.accelerometer.xml \
+    frameworks/native/data/etc/android.hardware.sensor.compass.xml:system/etc/permissions/android.hardware.sensor.compass.xml \
+    frameworks/native/data/etc/android.hardware.sensor.light.xml:system/etc/permissions/android.hardware.sensor.light.xml \
+    frameworks/native/data/etc/android.hardware.sensor.proximity.xml:system/etc/permissions/android.hardware.sensor.proximity.xml \
+    frameworks/native/data/etc/android.hardware.telephony.gsm.xml:system/etc/permissions/android.hardware.telephony.gsm.xml \
+    frameworks/native/data/etc/android.hardware.touchscreen.multitouch.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.xml \
+    frameworks/native/data/etc/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml \
+    frameworks/native/data/etc/android.hardware.wifi.direct.xml:system/etc/permissions/android.hardware.wifi.direct.xml \
+    frameworks/native/data/etc/android.software.sip.xml:system/etc/permissions/android.software.sip.xml \
+    frameworks/native/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml \
+    frameworks/native/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml \
+    frameworks/native/data/etc/android.hardware.usb.accessory.xml:system/etc/permissions/android.hardware.usb.accessory.xml
 # Audio
 PRODUCT_PACKAGES += \
-	audio.primary.msm7x27a \
-	audio.primary.default \
-   	audio_policy.msm7x27a \
-    	audio_policy.default \
-    	audio.a2dp.default \
-    	audio_policy.conf \
-    	libaudioutils
+    audio.primary.msm7x27a \
+    audio.primary.default \
+    audio_policy.msm7x27a \
+    audio_policy.default \
+    audio.a2dp.default \
+    audio_policy.conf \
+    libaudioutils
   
 # Video decoding
 PRODUCT_PACKAGES += \
-    	libmm-omxcore \
-    	libstagefrighthw \
-    	libOmxCore \
-    	libI420colorconvert \
-    	libdashplayer \
-    	qcmediaplayer
-  
+    libmm-omxcore \
+    libstagefrighthw \
+    libOmxCore \
+    libI420colorconvert \
+    libdashplayer \
+    qcmediaplayer
+
 # HW
 PRODUCT_PACKAGES += \
-    	libgenlock \
-    	liboverlay \
-    	gralloc.msm7x27a \
-    	hwcomposer.msm7x27a \
-    	copybit.msm7x27a \
-    	libtilerenderer \
-    	libqdMetaData
+    libgenlock \
+    liboverlay \
+    gralloc.msm7x27a \
+    hwcomposer.msm7x27a \
+    copybit.msm7x27a \
+    libtilerenderer \
+    libqdMetaData
 
 
 # Bluetooth
 PRODUCT_PACKAGES += \
-    	hciconfig \
-    	hcitool
-
-# Misc
-PRODUCT_PACKAGES += \
-    	com.android.future.usb.accessory
+    hciconfig \
+    hcitool
 
 # Live Wallpapers
 PRODUCT_PACKAGES += \
-    	LiveWallpapers \
-    	LiveWallpapersPicker \
-    	MagicSmokeWallpapers \
-    	HoloSpiralWallpaper \
-    	VisualizationWallpapers \
-    	librs_jni
+    LiveWallpapers \
+    LiveWallpapersPicker \
+    MagicSmokeWallpapers \
+    HoloSpiralWallpaper \
+    VisualizationWallpapers \
+    librs_jni
 
 # Power HAL
 PRODUCT_PACKAGES += \
-    	power.msm7x27a \
-    	libsurfaceflinger_client
+    power.msm7x27a \
+    libsurfaceflinger_client
 	
 # Lights HAL
 PRODUCT_PACKAGES += \
-    	lights.msm7x27a
+    lights.msm7x27a
 	
 # GPS
 PRODUCT_PACKAGES += \
-    	gps.default
+    gps.default
     
 # Camera
 PRODUCT_PACKAGES += \
-    	camera.msm7x27a	
+    camera.msm7x27a	
 
 # Wi-Fi
 PRODUCTS_PACKAGES += \
-        ath6kl_sdio
+    ath6kl_sdio
 
 # Other
 PRODUCT_PACKAGES += \
-        dexpreopt \
-        librpc \
-        com.android.future.usb.accessory \
-        libnetcmdiface
+    dexpreopt \
+    librpc \
+    com.android.future.usb.accessory \
+    libnetcmdiface
 
 # Include non-opensource parts
 $(call inherit-product, vendor/samsung/delos3geur/delos3geur-vendor.mk)
 $(call inherit-product, frameworks/native/build/phone-hdpi-512-dalvik-heap.mk)
+
+PRODUCT_AAPT_CONFIG := normal hdpi
+PRODUCT_AAPT_PREF_CONFIG := hdpi
+PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
+PRODUCT_TAGS += dalvik.gc.type-precise
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.setupwizard.enable_bypass=1 \
+    ro.telephony.call_ring.multiple=false \
+    ro.vold.umsdirtyratio=50 \
+    persist.sys.purgeable_assets=1 \
+    ro.telephony.call_ring.delay=3000
