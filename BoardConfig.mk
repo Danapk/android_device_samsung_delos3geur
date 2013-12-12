3e4c34641c599c38551968361c382e7eb1e15c01
Arnav Gosain <nolinuxnoparty@gmail.com>
2013-12-12 13:23:51 +0000

    delos3geur; remove ril hal

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -128,7 +128,6 @@ BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/samsung/delos3geur/bluetoo
 
 # RIL
 BOARD_MOBILEDATA_INTERFACE_NAME = "pdp0"
-BOARD_RIL_CLASS := ../../../device/samsung/delos3geur/libril/
 BOARD_USES_LIBSECRIL_STUB := true
 
 # Recovery
