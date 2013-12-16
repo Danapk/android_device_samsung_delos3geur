a07fc6b40498f84cbd43438ae8f4c96a9fe69322
k2wl <kartik838@yahoo.co.in>
2013-12-16 09:01:30 +0530

    correct the path for fstab

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -132,7 +132,7 @@ BOARD_USES_LIBSECRIL_STUB := true
 # Recovery
 BOARD_HAS_NO_MISC_PARTITION := true
 BOARD_HAS_NO_SELECT_BUTTON := true
-TARGET_RECOVERY_FSTAB := device/samsung/delos3geur/fstab.delos3geur
+TARGET_RECOVERY_FSTAB := device/samsung/delos3geur/recovery/fstab.delos3geur
 RECOVERY_FSTAB_VERSION := 2
 BOARD_CUSTOM_GRAPHICS := ../../../device/samsung/delos3geur/recovery/graphics.c
 TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
